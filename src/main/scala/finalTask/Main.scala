1da1af54f949c4ae7bd970dce9d012664cdb1d43
Roy <daimon--@live.ru>
2023-08-03 01:03:55 +0300

    Final Task in work

--- src/main/scala/finalTask/Main.scala
+++ src/main/scala/finalTask/Main.scala
@@ -13,25 +13,26 @@ object Main extends App{
 
   object Default{
     val TopCount: Int = 10
+    val CurrencyList: Set[String] = Set("Rub","Usd","Eur")
+    implicit val DefaultCurrency = "Rub"
   }
 
-  implicit val DefaultCurrency = "Rub"
+  import Default.DefaultCurrency
 
 
   val txtReader = new TxtReader[ExpensesFromFile](FileRule.f)
 
   val testData: Seq[ExpensesFromFile] = txtReader.read("src/main/resources/expenses.txt")
 
-  val currencyList: Set[String] = Set("Rub","Usd","Eur")
 
-  val parserProvider = new ParserProvider(currencyList)
+  val parserProvider = new ParserProvider(Default.CurrencyList)
 
 
   val convertedData = testData.map(
     x => parserProvider.pars[ExpensesFromFile, Expenses](x)
   )
 
-  convertedData.foreach(println)
+
 
   val totalExpensesFunc: Seq[Expenses] => SingleResult = {
     x =>
@@ -62,19 +63,13 @@ object Main extends App{
       )
   }
 
-
-
   val leadingCategoryFunc: Seq[Expenses] => SingleResult = { x =>
     SingleResult {
       val expensesByCategory: Map[String, Seq[Expenses]] = x.groupBy(_.expense_category)
-
-      // Вычисление сумм расходов для каждой категории
       val expensesSumByCategory: MapView[String, Currency] = expensesByCategory.mapValues { expensesForCategory =>
         val totalAmount = expensesForCategory.map(_.currency.amount).sum
         Currency(totalAmount, expensesForCategory.head.currency.designation)
       }
-
-      // Поиск категории с наибольшей суммой расходов
       val leadingCategory: (String, Currency) = expensesSumByCategory.maxBy { case (_, currency) => currency.amount }
 
       s"Категория-лидер по расходам: ${leadingCategory._1}, сумма: ${leadingCategory._2}"
